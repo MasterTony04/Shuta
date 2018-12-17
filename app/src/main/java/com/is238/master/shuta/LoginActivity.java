4291def60aeb787831d60f64a50a9a92aae0a684
MasterTony04 <mastertony04@gmail.com>
2018-12-17 09:26:05 +0300

    changed the theme, removed the Tab bar( Title bar) changed the button layout

--- app/src/main/java/com/is238/master/shuta/LoginActivity.java
+++ app/src/main/java/com/is238/master/shuta/LoginActivity.java
@@ -1,15 +1,23 @@
 package com.is238.master.shuta;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
+
     }
+
+
+
 
 
 }
