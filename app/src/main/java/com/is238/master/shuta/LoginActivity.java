e42d4df4dda84e3acb0e2fe7f65c693bbd574414
mastertony04 <mastertony04@gmail.com>
2018-12-23 23:36:50 +0300

    added icon to password field and reg number field in loginActivity
    added resetpassword activity plus its intent
    added landing page activity plus its intent

--- app/src/main/java/com/is238/master/shuta/LoginActivity.java
+++ app/src/main/java/com/is238/master/shuta/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.is238.master.shuta;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,7 +20,14 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
+    public void loginButton(View view) {
 
+        Intent landingPageIntent = new Intent(LoginActivity.this,LandingPageActivity.class);
+        startActivity(landingPageIntent);
+    }
 
-
+    public void resetPassword(View view) {
+        Intent resetPasswordIntent = new Intent(LoginActivity.this,PasswordReset.class);
+        startActivity(resetPasswordIntent);
+    }
 }
