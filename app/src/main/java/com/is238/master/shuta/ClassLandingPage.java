1854af609378f1217419d98aa0e0af43af7926f5
lilian <lilianbaitu@gmail.com>
2019-01-04 18:12:30 +0300

    Merge branch 'master' of /home/lilian/AndroidStudioProjects/Shuta with conflicts.

---
+++ app/src/main/java/com/is238/master/shuta/ClassLandingPage.java
@@ -0,0 +1,18 @@
+package com.is238.master.shuta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class ClassLandingPage extends AppCompatActivity {
+ ListView classlist;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_landing_page);
+    }
+
+    public ListView getClasslist() {
+        return classlist;
+    }
+}
