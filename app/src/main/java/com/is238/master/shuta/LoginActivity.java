9582dadfc9dbe010c799f472c4c332c8e04b7ca8
nnkogift <nnkogift@gmail.com>
2019-01-04 17:24:57 +0300

    Login button is now working

--- app/src/main/java/com/is238/master/shuta/LoginActivity.java
+++ app/src/main/java/com/is238/master/shuta/LoginActivity.java
@@ -1,29 +1,53 @@
 package com.is238.master.shuta;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
-
+        EditText reg_no_text, passwordTxt;
+        Button loginBtn;
+        CheckBox rememberMeChkBx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
-
-
-
+        init();
     }
 
 
     public void loginButton(View view) {
 
-        Intent landingPageIntent = new Intent(LoginActivity.this,StudentLandingPage.class);
+        Intent landingPageIntent;
+
+        String username = reg_no_text.getText().toString();
+        String password = passwordTxt.getText().toString();
+
+        if(username.equals("2016-04-01686") && password.equals("tonynikiazi")){
+            landingPageIntent  = new Intent(LoginActivity.this,StudentLandingPage.class);
+        }
+        else if (username.equals("123456789") && password.equals("teacher")){
+            landingPageIntent = new Intent(LoginActivity.this,StaffLandingActivity.class);
+        }
+        else {
+            landingPageIntent = new Intent(LoginActivity.this,LandingPageActivity.class);
+        }
+
+
         startActivity(landingPageIntent);
+    }
+
+    private void init(){
+        reg_no_text = findViewById(R.id.regNo);
+        passwordTxt = findViewById(R.id.password);
+        loginBtn =  findViewById(R.id.loginButton);
+        rememberMeChkBx = findViewById(R.id.remberMecheckBox);
     }
 
     public void resetPassword(View view) {
