1b6d43dccb37eb5ab2a372a554a911d51321c07b
lilian <lilianbaitu@gmail.com>
2019-01-11 11:17:17 +0300

    all class Activites

--- app/src/main/java/com/is238/master/shuta/AddClassActivity.java
+++ app/src/main/java/com/is238/master/shuta/AddClassActivity.java
@@ -1,18 +1,13 @@
 package com.is238.master.shuta;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class AdminActivity extends AppCompatActivity {
+public class AddClassActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_admin);
-
+        setContentView(R.layout.activity_add_class);
     }
-
 }
--- app/src/main/java/com/is238/master/shuta/ClassLandingPage.java
+++ app/src/main/java/com/is238/master/shuta/ClassLandingPage.java
@@ -2,15 +2,22 @@ package com.is238.master.shuta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class ClassLandingPage extends AppCompatActivity {
- ListView classlist;
+ private static ListView classlist;
+
+    // Array of strings...
+    String formclass[] = {"Form 1A", "Form 2B", "FORM 3A", "FORM 4A"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_landing_page);
+
+        classlist = (ListView)findViewById(R.id.list_class);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_class_landing_page, R.id.textView, formclass);
+        classlist.setAdapter(arrayAdapter);
     }
-
-
 }
