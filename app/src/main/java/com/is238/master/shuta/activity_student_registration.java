45f171b92c2448eab8e2d7e13b29aafb2a7eba53
james mkerenga <mkerengajames@gmail.com>
2019-01-17 02:13:49 +0300

    student registration form created

---
+++ app/src/main/java/com/is238/master/shuta/activity_student_registration.java
@@ -0,0 +1,13 @@
+package com.is238.master.shuta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_student_registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_student_registration);
+    }
+}
