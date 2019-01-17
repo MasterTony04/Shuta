47220ceaf48e12c4c1bfdabbdcd559706b9bf712
lilian <lilianbaitu@gmail.com>
2019-01-17 22:55:00 +0300

    subjectlandingpage

---
+++ app/src/main/java/com/is238/master/shuta/SubjectLandingPage.java
@@ -0,0 +1,17 @@
+package com.is238.master.shuta;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SubjectLandingPage extends AppCompatActivity {
+private static Button subjectButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subject_landing_page);
+
+    }
+
+}
