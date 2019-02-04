f7510fe9cc080f0e6e166817ca6c663ecc710fc4
lilian <lilianbaitu@gmail.com>
2019-02-04 08:23:34 +0300

    subjectlandingpage

--- app/src/main/java/com/is238/master/shuta/StaffUploadResult.java
+++ app/src/main/java/com/is238/master/shuta/StaffUploadResult.java
@@ -1,16 +1,13 @@
 package com.is238.master.shuta;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class SubjectLandingPage extends AppCompatActivity {
-private static Button subjectButton;
+public class StaffUploadResult extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_subject_landing_page);
-
+        setContentView(R.layout.activity_staff_upload_result);
     }
 }
