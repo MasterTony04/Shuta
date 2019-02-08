cb860dfff4a6768596fae332776ed484cc750550
nnkogift <nnkogift@gmail.com>
2019-02-08 16:00:52 +0300

    Negotiations

--- app/src/main/java/com/is238/master/shuta/SubjectListActivity.java
+++ app/src/main/java/com/is238/master/shuta/SubjectListActivity.java
@@ -2,12 +2,23 @@ package com.is238.master.shuta;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class SubjectListActivity extends AppCompatActivity {
+    ListView subjectList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_list);
+    }
+
+    protected void init(){
+
+    subjectList.findViewById(R.id.list_subject);
+
+
+
     }
 }
