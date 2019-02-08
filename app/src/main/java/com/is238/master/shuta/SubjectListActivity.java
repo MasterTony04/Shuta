44b895eaa44646ebd71e5735b79d33a30a5a7306
lilian <lilianbaitu@gmail.com>
2019-02-08 16:51:56 +0300

    added subjectlist and floatinbtn

--- app/src/main/java/com/is238/master/shuta/SubjectListActivity.java
+++ app/src/main/java/com/is238/master/shuta/SubjectListActivity.java
@@ -1,13 +1,29 @@
 package com.is238.master.shuta;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SubjectListActivity extends AppCompatActivity {
+
+   private FloatingActionButton subjAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_list);
+
+        subjAdd=findViewById(R.id.addSubject);
+
+        subjAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newSubject = new Intent(SubjectListActivity.this,SubjectLandingPage.class);
+                startActivity(newSubject);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/is238/master/shuta/activity_teacher_form.java
+++ app/src/main/java/com/is238/master/shuta/activity_teacher_form.java
@@ -1,13 +1,18 @@
 package com.is238.master.shuta;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class activity_teacher_form extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_form);
+
+
     }
 }
